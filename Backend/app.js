eb3e3ae9220824e584f08c9a47029e5be78ae39e
Saikat Biswas <67178868+saikat1236@users.noreply.github.com>
2021-07-16 09:18:15 +0530

    adwd

--- Backend/app.js
+++ Backend/app.js
@@ -12,12 +12,23 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 // app.use('/', require('./routes/news1'))
 app.use("/", require("./routes/news"));
-app.get("/news", (req, res) => {
+
+app.get("/news/business", (req, res) => {
   //   res.sendFile(path.join(__dirname, "/views/news1.ejs"));
   // res.send({ name: "GeeksforGeeks" });
   // res.sendFile(path.resolve("views/news1.ejs"));
-  res.sendFile("views/news.ejs", { root: __dirname });
+  res.sendFile("pages/business.html", { root: __dirname });
 });
+app.get("/news/health", (req, res) => {
+  res.sendFile("pages/health.html", { root: __dirname });
+});
+app.get("/news/world", (req, res) => {
+  res.sendFile("pages/world.html", { root: __dirname });
+});
+app.get("/news/sports", (req, res) => {
+  res.sendFile("pages/sports.html", { root: __dirname });
+});
+
 app.get("/news1", (req, res) => {
   res.sendFile("views/news1.html", { root: __dirname });
 });
@@ -28,6 +39,7 @@ app.get("/news3", (req, res) => {
   res.sendFile("views/news3.html", { root: __dirname });
 });
 app.set('views', './views')
+app.set("pages", "./pages");
 // app.set("views", path.join(__dirname, "views"));
 // app.engine("html", require("ejs").renderFile);
 app.listen(port, () => console.log("started"))
